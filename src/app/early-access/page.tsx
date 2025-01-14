6a78dc2034c01c378cdca67386faf419ed2b1c3f
Jean-Marc R-Mahoro <jean_marcc@hotmail.com>
2025-01-13 21:32:09 -0400

    nearly integrated stripe, few steps left

--- src/app/early-access/page.tsx
+++ src/app/early-access/page.tsx
@@ -1,4 +1,19 @@
+"use client";
+
+import CheckoutPage from "../../../components/CheckoutPage";
+import convertToSubcurrency from "../../../lib/convertToSubcurrency";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
+  throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not set");
+}
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
 export default function EarlyAccess() {
+  const amount = 49.99;
+
   return (
     <div className="min-h-screen bg-gradient-to-b from-black via-[#0a0a0a] to-[#171717] relative overflow-hidden">
       {/* Background Pattern */}
@@ -9,11 +24,23 @@ export default function EarlyAccess() {
       <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-purple-500/20 rounded-full filter blur-[128px] -z-10"></div>
 
       <section className="relative pt-32 pb-16 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
-        <h1 className="text-4xl sm:text-5xl font-bold text-center mb-16">
-          Early Access
-        </h1>
+        <h1 className="text-4xl sm:text-5xl font-bold text-center mb-16">JM</h1>
+        <h2 className="text-2xl sm:text-3xl font-bold text-center mb-16">
+          has requested <span className="text-green-500">${amount}</span>
+        </h2>
         {/* Add early access content here */}
       </section>
+
+      <Elements
+        stripe={stripePromise}
+        options={{
+          mode: "payment",
+          amount: convertToSubcurrency(amount), //cents
+          currency: "usd",
+        }}
+      >
+        <CheckoutPage />
+      </Elements>
     </div>
   );
 }
